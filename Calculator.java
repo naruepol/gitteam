b095a73c81ed837c91834d5b64a1f8a0363d12f5
somkiat <somkiat@kru.ac.th>
2020-09-03 16:19:45 +0700

    Use Class 2

--- Calculator.java
+++ Calculator.java
@@ -6,9 +6,9 @@ public class Calculator{
 		return num1*num2;
 	}
 	public int subtract(int num1, int num2){
-		return 0;
+		return num1-num2;
 	}
 	public double divide(int num1, int num2){
-		return 0;
+		return num1/num2;
 	}
 }
--- CalculatorDriver.java
+++ CalculatorDriver.java
@@ -3,5 +3,7 @@ public class CalculatorDriver{
 		Calculator c1 = new Calculator();
 		System.out.println(c1.add(9,7));
 		System.out.println(c1.multiply(9,9));
+		System.out.println(c1.subtract(9,7));
+		System.out.println(c1.divide(9,9));
 	}
 }
